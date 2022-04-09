a3828036b3c339396fd1d40fec156288b2d9bbec
Ayoub karam <karamossayoub1999@gmail.com>
2022-04-09 08:39:57 +0000

    first commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,8 @@
+@extends('posts.master')
+@section('title')
+    Post show
+@endsection
+@section('content')
+    <h1>This is Post Show page an id: {{ $post->id}} and title is: {{ $post->title}}!</h1>
+    
+@endsection
